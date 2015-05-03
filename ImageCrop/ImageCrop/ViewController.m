0a98b2d6185b8b5391b95a42947415b5d6bc2ac6
bruce <bruce@brucesair-2.local>
2015-05-03 15:11:38 +0800

    设置status bar 为浅色主题

--- ImageCrop/ImageCrop/ViewController.m
+++ ImageCrop/ImageCrop/ViewController.m
@@ -107,6 +107,11 @@
 
 }
 
+-(UIStatusBarStyle)preferredStatusBarStyle
+{
+    return UIStatusBarStyleLightContent;
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
