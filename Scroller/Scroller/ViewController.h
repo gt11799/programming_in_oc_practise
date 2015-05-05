dd34c8abafba27916a46abb47ea0eb2fef7a9e7c
bruce <bruce@brucesair-2.local>
2015-05-05 08:23:55 +0800

    practise scroller

---
+++ Scroller/Scroller/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Scroller
+//
+//  Created by bruce on 15/5/5.
+//  Copyright (c) 2015年 bruce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Scroller/Scroller/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  Scroller
+//
+//  Created by bruce on 15/5/5.
+//  Copyright (c) 2015年 bruce. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIScrollView *theScroller;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+-(void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    self.theScroller.contentSize = CGSizeMake(280.0, 1136.0);
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
